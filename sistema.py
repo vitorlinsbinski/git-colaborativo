9260cea77bc819a82355c92800003e95f46380d4
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2025-08-19 13:18:28 -0400

    feat: adiciona login de usuário

--- sistema.py
+++ sistema.py
@@ -4,7 +4,15 @@ def exibir_dashboard():
 def cadastrar_usuario(nome):
     print(f"Usuário {nome} cadastrado com sucesso!")
 
+def login(usuario, senha):
+    print(f"Tentando logar usuário {usuario}...")
+    if senha == "123":
+        print("Login bem-sucedido!")
+    else:
+        print("Senha incorreta.")
+
 def main():
     print("Sistema iniciado")
     exibir_dashboard()
     cadastrar_usuario("Maria")
+    login("Vitor", "123")
